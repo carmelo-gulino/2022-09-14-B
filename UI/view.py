6d3895fe55f885b9a09061da89f28289524116a5
Carmelo Gulino <s298109@studenti.polito.it>
2024-06-25 09:17:10 +0200

    completo

---
+++ UI/view.py
@@ -0,0 +1,46 @@
+import flet as ft
+
+
+class View(ft.UserControl):
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        # page stuff
+        self.page = page
+        self.page.title = "Template application using MVC and DAO"
+        self.page.horizontal_alignment = 'CENTER'
+        self.page.theme_mode = ft.ThemeMode.DARK
+        # controller (it is not initialized. Must be initialized in the main, after the controller is created)
+        self._controller = None
+        # graphical elements
+        self._title = None
+        self.txt_result = None
+
+    def load_interface(self):
+        # title
+        self._title = ft.Text("Hello World", color="blue", size=24)
+        self.page.controls.append(self._title)
+
+        # List View where the reply is printed
+        self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self.page.controls.append(self.txt_result)
+        self.page.update()
+
+    @property
+    def controller(self):
+        return self._controller
+
+    @controller.setter
+    def controller(self, controller):
+        self._controller = controller
+
+    def set_controller(self, controller):
+        self._controller = controller
+
+    def create_alert(self, message):
+        dlg = ft.AlertDialog(title=ft.Text(message))
+        self.page.dialog = dlg
+        dlg.open = True
+        self.page.update()
+
+    def update_page(self):
+        self.page.update()
